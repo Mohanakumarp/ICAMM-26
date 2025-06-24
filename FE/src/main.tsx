123abce8ae21ff4d8f7218828539573e953c0a47
Mohana Kumar P <148449818+Mohanakumarp@users.noreply.github.com>
2025-06-24 09:40:08 +0000

    Fix root element ID in index.html, remove unused React import in Home component, and add JSX support in tsconfig.json

--- FE/src/main.tsx
+++ FE/src/main.tsx
@@ -3,16 +3,20 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home/home';
+
 const rootElement = document.getElementById('root');
-  <StrictMode>
-    <Router>
-      {/* <Navigation /> - You need to import or create this component */}
-      {/* <ChatBubble/> */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* Add your routes here */}
-      </Routes>
-    </Router>
-  </StrictMode>
 
-
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <Router>
+        {/* <Navigation /> - You need to import or create this component */}
+        {/* <ChatBubble/> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* Add your routes here */}
+        </Routes>
+      </Router>
+    </StrictMode>
+  );
+}
