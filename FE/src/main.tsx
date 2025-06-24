50cc8916b3c08c2133c16fc9f233294b4b92cb7e
Mohanakumarp <mohanakumarp2828@gmail.com>
2025-06-24 09:03:00 +0530

    Add React setup with Home component and update dependencies

---
+++ FE/src/main.tsx
@@ -0,0 +1,18 @@
+import './style.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home/home';
+const rootElement = document.getElementById('root');
+  <StrictMode>
+    <Router>
+      {/* <Navigation /> - You need to import or create this component */}
+      {/* <ChatBubble/> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* Add your routes here */}
+      </Routes>
+    </Router>
+  </StrictMode>
+
+
